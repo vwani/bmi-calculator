854df4a789faa2c7631fea287c9c52c49c93a5d3
Vinaya Wani <93176721+vwani@users.noreply.github.com>
2023-12-24 19:35:56 +0530

    Update bmi_calculator.py
    Converted entire code into OOP. 
    Encapsulated all code under " if __name__ == '__main__' " block into the function calculator() of the bmiCalculator class.

--- bmi_calculator.py
+++ bmi_calculator.py
@@ -1,49 +1,55 @@
 from tkinter import *
 
-
 class bmiCalculator:
+    
     def __init__(self):
+
+        self.main = Tk()
+        self.main.title('BMI Calculator')
+        self.main.geometry('800x800')
+        
         self.age = DoubleVar()
         self.height = DoubleVar()
         self.weight = DoubleVar()
         self.bmi = DoubleVar()
 
-    def calculate(self):
-        self.bmi = float(self.weight.get())/(float(self.height.get())*float(self.height.get()))
-        bmiLabel.config(text = 'Your BMI is {}'.format(self.bmi))
-        bmiLabel.place(relx = 0.2, rely = 0.5)
+        self.ageLabel = Label(self.main, text = 'Age:')
+        self.ageEntry = Entry(self.main, textvariable = self.age, cursor = 'xterm', justify = CENTER)
+
+        self.heightLabel = Label(self.main, text = 'Height:')
+        self.heightEntry = Entry(self.main, textvariable = self.height, cursor = 'xterm', justify = CENTER)
+
+        self.weightLabel = Label(self.main, text = 'Weight:')
+        self.weightEntry = Entry(self.main, textvariable = self.weight, cursor = 'xterm', justify = CENTER)
+
+        self.calcButton = Button(self.main, text = 'Calculate', cursor = 'hand2', command = self.calculate)
+
+        self.bmiLabel = Label(self.main)        
+
+    def calculator(self):
         
+        self.ageLabel.place(relx = 0.2, rely = 0.1)
+        self.ageEntry.place(relx = 0.3, rely = 0.1)
+
+        self.heightLabel.place(relx = 0.2, rely = 0.2)
+        self.heightEntry.place(relx = 0.3, rely = 0.2)
+
+        self.weightLabel.place(relx = 0.2, rely = 0.3)
+        self.weightEntry.place(relx = 0.3, rely = 0.3)
+
+        self.calcButton.place(relx = 0.25, rely = 0.4)
+
+        self.main.mainloop()
+
+    def calculate(self):
+        
+        self.bmi = float(self.weight.get())/(float(self.height.get())*float(self.height.get()))
+        self.bmiLabel.config(text = 'Your BMI is {}'.format(self.bmi))
+        self.bmiLabel.place(relx = 0.2, rely = 0.5)
 
 if __name__ == '__main__':
     
-    main = Tk()
-    main.title('BMI Calculator')
-    main.geometry('800x800')
-
     bmiCalc = bmiCalculator()
-
-    ageLabel = Label(main, text = 'Age:')
-    ageLabel.place(relx = 0.2, rely = 0.1)
-
-    ageEntry = Entry(main, textvariable = bmiCalc.age, cursor = 'xterm', justify = CENTER)
-    ageEntry.place(relx = 0.3, rely = 0.1)
-
-    heightLabel = Label(main, text = 'Height:')
-    heightLabel.place(relx = 0.2, rely = 0.2)
-
-    heightEntry = Entry(main, textvariable = bmiCalc.height, cursor = 'xterm', justify = CENTER)
-    heightEntry.place(relx = 0.3, rely = 0.2)
-
-    weightLabel = Label(main, text = 'Weight:')
-    weightLabel.place(relx = 0.2, rely = 0.3)
-
-    weightEntry = Entry(main, textvariable = bmiCalc.weight, cursor = 'xterm', justify = CENTER)
-    weightEntry.place(relx = 0.3, rely = 0.3)
-
-    calcButton = Button(main, text = 'Calculate', cursor = 'hand2', command = bmiCalc.calculate)
-    calcButton.place(relx = 0.25, rely = 0.4)
-
-    bmiLabel = Label(main)
+    bmiCalc.calculator()
 
 
-    main.mainloop()
